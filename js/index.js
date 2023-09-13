6447f9ce23563c0a64367b63674d95394d794c20
hy3zzz <96004470+hy3won@users.noreply.github.com>
2023-09-13 10:49:57 +0900

    ㅇㅇ

--- js/index.js
+++ js/index.js
@@ -1,42 +1,36 @@
 $(document).ready(function () {
-  // 첫 로딩 시 오른쪽 페이지의 content를 0으로 설정
-  let initialView = $("#book").turn("view");
-  if (initialView[1]) {
-    $(".page:eq(" + (initialView[1] - 1) + ") .content").css("opacity", 0);
-  }
+  // 초기에 content 숨기기
+  $(".content").css("opacity", 0);
 
   $("#book").bind("turning", function (event, page, view) {
-    var audioElement = document.getElementById("pageTurnSound");
-    audioElement.currentTime = 0; // 오디오를 시작부터 재생
-    audioElement.play();
+    // 모든 .content 요소의 opacity를 0으로 설정
+    $(".content").css("opacity", 0);
+  });
 
-    //   if (view[1]) {
-    //     $(".page:eq(" + (view[1] - 1) + ") .content").css("opacity", 0);
-    //   }
-    // });
+  $("#book").bind("turned", function (event, page, view) {
+    function fadeInContentOfPage(pageNumber) {
+      var pageContentElements = $(
+        "#book .page[data-page-number='" + pageNumber + "'] .content"
+      );
+      pageContentElements.each(function (index, element) {
+        setTimeout(function () {
+          $(element).animate({ opacity: 1 }, 500);
+        }, 500 * (index + 1));
+      });
+    }
 
-    // $("#book").bind("turned", function (event, page, view) {
-    //   function fadeInContentOfPage(pageNumber) {
-    //     var pageContentElements = $(`#book .page:eq(${pageNumber - 1}) .content`);
+    if (view[0]) {
+      console.log("Left Page Number:", view[0]);
+      fadeInContentOfPage(view[0]);
+    }
 
-    //     pageContentElements.each(function (index, element) {
-    //       setTimeout(function () {
-    //         $(element).animate({ opacity: 1 }, 500); // 투명도를 천천히 1로 변화
-    //       }, 500 * (index + 1));
-    //     });
-    //   }
-
-    //   // 좌측 페이지의 content는 무조건 표시
-    //   fadeInContentOfPage(view[0]);
-
-    //   // 오른쪽 페이지가 있으면 그 페이지의 content 표시
-    //   if (view[1]) {
-    //     var delayForLeftPage =
-    //       500 * $(".page:eq(" + (view[0] - 1) + ") .content").length;
-    //     setTimeout(function () {
-    //       fadeInContentOfPage(view[1]);
-    //     }, delayForLeftPage);
-    //   }
+    if (view[1]) {
+      console.log("Right Page Number:", view[1]);
+      setTimeout(function () {
+        fadeInContentOfPage(view[1]);
+      }, 500 *
+        $("#book .page[data-page-number='" + view[0] + "'] .content").length);
+    }
   });
 
   $("#book").turn({ gradients: true, acceleration: true });
